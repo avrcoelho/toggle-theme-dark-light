7722a35233f7ce969ff9bc84e8f33a0448f03e4f
André Coelho <andre.ribeiro@iv2.com.br>
2020-02-03 13:05:43 -0300

    Toggle Theme

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,9 @@
+export default {
+  title: "dark",
+  colors: {
+    primary: "#c62E65",
+    secundary: "#D63AF9",
+    background: "#222",
+    text: "#fff"
+  }
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,9 @@
+export default {
+  title: "light",
+  colors: {
+    primary: "#c62E65",
+    secundary: "#D63AF9",
+    background: "#f5f5f5",
+    text: "#333"
+  }
+};
